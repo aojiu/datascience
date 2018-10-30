4c1fb6748161d629390ab1f314f118132ab5d5a0
Vic_xie <39664379+aojiu@users.noreply.github.com>
2018-10-29 20:52:28 -0400

    Update tweet.py

--- tweet.py
+++ tweet.py
@@ -1,13 +1,13 @@
 import tweepy
 import textblob
 
-cosumer_key = "K7s0RNIAlAGNgKOAkkniXMCJ8"
-consumer_sec = "ee82MGBbeaJMZmzp7oAwuHDEmQjbFp4b33UE4EW4UiF7CRxsWS"
+cosumer_key = "PvmjejUXuDBGuzdVxqwsw3Q3R"
+consumer_sec = "DwGsrHBIB4uIG0MPeUZdOfZ8DYRxczHRgQplqeP58OrQQ3tBvF"
 
-access_key = "879872311653957633-KN56c9Nk71dHMaKwjjAulQxf2cJVun1"
-access_secret = "iq13PxEzKzSwqFVUXqgxTwqpVDRr8s6NZgql8ixh8dPIT"
+access_key = "879872311653957633-qZKhfCJKiZn4AaMpSjQPEZXr8Pp2hbM"
+access_secret = "hsVnFq4PFE51CGp3kTgJUdxm05CqEMOtQjYoxRiIMsPMq"
 
-auth = tweepy.OAuthHandler(consumer_sec, consumer_sec)
+auth = tweepy.OAuthHandler(cosumer_key, consumer_sec)
 auth.set_access_token(access_key, access_secret)
 
 api = tweepy.API(auth)
@@ -16,5 +16,5 @@ public_tweets = api.search("Nintendo")
 
 for tweet in public_tweets:
     print(tweet.text)
-    analysis = textblob(tweet.text)
-    print(analysis.sentiment)
+    # analysis = textblob(tweet.text)
+    # print(analysis.sentiment)
